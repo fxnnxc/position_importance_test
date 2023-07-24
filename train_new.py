e2a6e3079c8a1f13a220c6796300846977451e66
jleehun <ehsgnsdms@gmail.com>
2023-07-24 16:45:41 +0000

    change batch, include arguments

--- train_new.py
+++ train_new.py
@@ -30,7 +30,20 @@ def save(model, dir, name):
     os.makedirs(dir, exist_ok=True)
     save_path = os.path.join(dir, f'{name}.pth')
     torch.save(model.state_dict(), save_path)
+
+parser = argparse.ArgumentParser(description='Fine-tune GPT-2 on your custom dataset.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
+parser.add_argument('--device', type=str, default='cuda:0')
+# parser.add_argument('--pos_name', type=str, default='sinusoidal')
+parser.add_argument('--optimizer', type=str, default='adam')
+
+parser.add_argument('--num-of-layers', type=int, default='2')
+                    
+
+
+args = parser.parse_args()
+print(args)
 
 seed = 888
 deterministic = False
@@ -39,21 +52,24 @@ random.seed(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
-if deterministic:
-	torch.backends.cudnn.deterministic = True
-	torch.backends.cudnn.benchmark = False
+# if deterministic:
+# 	torch.backends.cudnn.deterministic = True
+# 	torch.backends.cudnn.benchmark = False
 
-device='cuda:0'
-learning_rate  =0.00002
-batch_size = 1
-epoch = 100
+device=args.device
+
+learning_rate=0.00002
+batch_size = 64
+epoch = 20
 save_every = 10
 
 
 date = datetime.today()            # 현재 날짜 가져오기
 
-save_dir = os.path.join('outputs/pretrained/', f'{datetime.today().month}_{datetime.today().day}')
-log_dir = f"outputs/log_dir/{datetime.today().month}_{datetime.today().day}" ### change
+save_dir = os.path.join('outputs/pretrained/', f"{args.num_of_layers}_{args.optimizer}")
+log_dir = f"outputs/log_dir/{args.num_of_layers}_{args.optimizer}"
+# save_dir = os.path.join('outputs/pretrained/', f'{datetime.today().month}_{datetime.today().day}')
+# log_dir = f"outputs/log_dir/{datetime.today().month}_{datetime.today().day}" ### change
 writer = SummaryWriter(log_dir)
 
 # dataset 
@@ -72,14 +88,17 @@ test_dataset = MathematicalShapesDataset(  # sum to 100
                                     return_rule_label=True)
 
 # load model
-config=GPT2Config(n_layer=2, vocab_size=train_dataset.vocab_len, eos_token_id=train_dataset.eos_token)
+config=GPT2Config(n_layer=args.num_of_layers, vocab_size=train_dataset.vocab_len, eos_token_id=train_dataset.eos_token)
 model = GPT2LMHeadModel(config=config).to(device)
 
 # select variables to update while training
 all_vars = [tensor for tensor in model.parameters()]
 
 # create optimizer
-optimizer = torch.optim.Adam(all_vars, lr=learning_rate)
+if args.optimizer == 'sgd':
+    optimizer = torch.optim.Adam(all_vars, lr=learning_rate)
+else:
+    optimizer = torch.optim.SGD(all_vars, lr=learning_rate)
 optimizer.zero_grad()
 
 # train
@@ -87,50 +106,56 @@ optimizer.zero_grad()
 # if not os.path.exists(save_dir):
 #     os.makedirs(save_dir)
 
-# with open(os.path.join(SAMPLE_DIR, args.run_name, 'samples-{}.txt'.format(counter)), 'w') as fp:
-        
+train_loader = DataLoader(train_dataset, batch_size)
+pbar = tqdm(train_loader, total=len(train_dataset)//batch_size)
+
+train_loss = []
 for i in range(epoch):
-    pbar = tqdm(range(len(train_dataset)))
     pbar.set_description(f" train - epoch {i+1}")
     avg = 0
+    total = 0
 
-    for idx in pbar:
-        data = train_dataset[idx]
-        data['input_ids'] = data['input_ids'].to(device)
-        data['labels'] = data['input_ids'].to(device)
-        # if idx > 1560:
-        #     print(data)
+    for idx, samples in enumerate(pbar):
+        data = {}
+        data['input_ids'] = samples['input_ids'].to(device)
+        data['labels'] = samples['input_ids'].to(device)
         output = model(**data)
         
         loss = output.loss
         loss.backward()
         optimizer.step()
         
-        avg += loss
-        avg = avg / (idx + 1)
+        total += loss
         
-        # writer.add_scalar('Loss/Train',loss,epoch*len(train_dataset) + idx)        
-        # pbar.set_postfix(avg_loss = avg.item())
-    writer.add_scalar('Loss/Train',avg,i)
+    avg = total / (len(train_dataset)//batch_size)
+    train_loss.append(avg.item())
+    writer.add_scalar(f'Loss/{args.num_of_layers}_{args.optimizer}/Train',avg,i)
+    
     if not(i % save_every) or (i == epoch - 1):
         save(model, save_dir, f'{i}')
+        
+    if i == 0:
+        best = avg
+    
+    if best >= avg:
+        save(model, save_dir, f'best_{i}')
+        
     
     # evaluation
-    model.eval()
-    optimizer.zero_grad()
-    print("Test for 5 random samples")
-    for _ in range(5):
-        index = np.random.randint(len(test_dataset))
-        i = test_dataset[index]
-        i['input_ids'] = i['input_ids'].to(device)
-        print(i) 
-        output = model.generate(i['input_ids'][:5].unsqueeze(0), max_new_tokens=3, min_new_tokens=3)
-        print(output) 
-            # fp.write('\n'.join(all_text))
-        
+    # model.eval()
+    # optimizer.zero_grad()
+    # print("Test for 5 random samples")
+    # for _ in range(5):
+    #     index = np.random.randint(len(test_dataset))
+    #     i = test_dataset[index]
+    #     i['input_ids'] = i['input_ids'].to(device)
+    #     print(i) 
+    #     output = model.generate(i['input_ids'][:5].unsqueeze(0), max_new_tokens=3, min_new_tokens=3)
+    #     print(output) 
+    #         # fp.write('\n'.join(all_text))
 
-    model.train()
-    optimizer.zero_grad()
+    # model.train()
+    # optimizer.zero_grad()
 
     
 
@@ -150,10 +175,8 @@ for idx in pbar:
     loss = output.loss
     
     avg += loss
-    avg = avg / (idx + 1)
-
-    pbar.set_postfix(avg_loss = avg)
-writer.add_scalar('Loss/Test',avg,idx)
+avg /= len(test_dataset)//batch_size
+writer.add_scalar(f'Loss/{args.num_of_layers}_{args.optimizer}/Test',avg,0)
 
 # -------------
 # for i in loader:
@@ -163,4 +186,10 @@ writer.add_scalar('Loss/Test',avg,idx)
 #     print(output) 
 #     break
 
+os.makedirs(f'outputs/loss', exist_ok=True)
+
+with open((f'outputs/loss/{args.num_of_layers}_{args.optimizer}.txt'), 'w') as fp:
+    fp.write(f'{args.num_of_layers}_{args.optimizer}\n')
+    fp.write(f'{train_loss}\n')
+    fp.write(f'{avg}')    
 
